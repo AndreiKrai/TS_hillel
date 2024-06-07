efe47830b2bc0ee7d0248eaef9fb4d0e902c614a
AndreiKrai <okraichenko.a@gmail.com>
2024-06-07 11:00:27 +0200

    all departments created

--- src/zoo.ts
+++ src/zoo.ts
@@ -8,8 +8,11 @@
 // Каса додає дані про відвідувача у два списки: поточні відвідувачі та клієнти.
 // "Поточні відвідувачі":
 //  Зберігає інформацію про відвідувачів, включаючи їхні імена та контактні дані.
+
 //  Можливість оповіщення відвідувачів за 15 хвилин до закриття і перед відходом.
+
 //  "Клієнти": Дані клієнтів зберігаються у Відділу реклами.
+
 //  Відділ реклами використовує цей список для розсилки новин про зоопарк і рекламні акції.
 //  "Відділ реклами": Відповідає за маркетингові та рекламні заходи.
 //  Використовує список клієнтів для розсилки новин про зоопарк і рекламні акції.
@@ -18,7 +21,9 @@
 //  закупівлею корму для тварин і обслуговуванням зоопарку. Зберігає дані про всіх співробітників, тварин і виплати.
 //  Можливість генерувати фінансові звіти.
 //  "Адміністрація": Відповідає за управління співробітниками і тваринами. Може додавати і видаляти співробітників і тварин.
+
 //  Створює сповіщення про рекламні акції та інші важливі події в зоопарку.
+
 //  "Тварини": Включає в себе інформацію про кожну тварину, таку як вид, ім'я, вік, здоров'я та інші характеристики.
 //  "Співробітники": Адміністрація може додавати і видаляти співробітників.
 //  Співробітники можуть мати різні посади та обов'язки, які слід враховувати.
@@ -26,45 +31,61 @@
 //  Можливість вести бюджетний облік і надавати фінансові звіти.
 
 enum TicketType {
-  EDULT = 5,
-  CHILD = 3,
-  FAMILY = 15,
+  ADULT = 'adult',
+  CHILD = 'child',
+  FAMILY = 'family',
+}
+const ticketPrice = {
+  [TicketType.ADULT]: 50,
+  [TicketType.CHILD]: 25,
+  [TicketType.FAMILY]: 100,
+};
+enum TransactionTypeEnum {
+  DEBET = 'debet',
+  CREDIT = 'credit',
+  KASSA = 'kassa',
+  SALARY = 'salary',
 }
 type AnimalHealthType = 'sick' | 'good' | 'exelent';
 class Kassa {
   //  "Клієнти": Дані клієнтів зберігаються у Відділу реклами. dont really want to add _addDepartment propetry here, should I?
   private _daylyIncome: number = 0;
-  public get daylyIncome() {
+  private _ticket: Ticket | null = null;
+
+  public get daylyIncome(): number {
     return this._daylyIncome;
   }
-  public close() {
+  public closeDay(): number {
+    // dont know how name it in another way, we use it to close day, entire day money put to account. call that dayly.
     const summ = this._daylyIncome;
     this._daylyIncome = 0;
     return summ;
   }
-  public increaseDaylyIncome(ammount: number): number {
-    return (this._daylyIncome += ammount);
+  public sellTicket(ticketType: TicketType, client: Person, clientList: Person[][]): void {
+    clientList.forEach(list => list.push(client));
+    this.issueTicket(ticketType);
+    this.increaseDaylyIncome(ticketType);
   }
-
-  sendTicket(client: Client, clientList: IClientList[]) {
-    clientList.forEach(list => list.addClient(client));
+  protected issueTicket(ticketType: TicketType): Ticket {
+    return (this._ticket = new Ticket(ticketType));
+  }
+  protected increaseDaylyIncome(ticketType: TicketType): number {
+    return (this._daylyIncome += ticketPrice[ticketType]);
   }
 }
 
-// class Ticket {
-//   public ticketType!: TicketType;
-//   private price!: number;
-
-//   constructor(ticketType: TicketType, price: number) {
-//     (this.price = price), (this.ticketType = ticketType);
-//   }
-// }
-
-class Client {
+class Ticket {
+  protected _ticketType!: TicketType;
+  protected _price!: number;
+  constructor(ticketType: TicketType) {
+    (this._price = ticketPrice[ticketType]), (this._ticketType = ticketType);
+  }
+}
+class Person {
   private readonly _id!: number;
   private _firstName!: string;
   private _lastName!: string;
-  private _contacts!: string;
+  private _contacts?: string;
   constructor(id: number, firstName: string, lastName: string, contacts: string) {
     ((this._id = id), (this._firstName = firstName)), (this._lastName = lastName), (this._contacts = contacts);
   }
@@ -78,67 +99,144 @@ class Client {
   public get id(): number {
     return this._id;
   }
-  public get contacts(): string {
+  public get contacts(): string | undefined {
     return this._contacts;
   }
 }
-interface IClientList {
-  list: Client[];
-  addClient(client: Client): void;
-  removeClent(client: Client): void;
-}
-class ClientList implements IClientList {
-  protected _list: Client[] = [];
-  get list(): Client[] {
-    return this._list;
-  }
-  addClient(client: Client) {
-    this._list.push(client);
-  }
-  removeClent(client: Client) {
-    this._list = this._list.filter(i => i.id !== client.id);
-  }
-}
+const clientsList: Person[] = [];
+const currentVisitorsList: Person[] = [];
 
-class CurrentVisitorsList extends ClientList {
-  clearList(): void {
-    this._list = [];
-  }
-}
 class MarketingDepartment {
   //  Використовує список клієнтів для розсилки новин про зоопарк і рекламні акції.
+  public sendAdvitisement(clientList: Person[], advertisement: string) {
+    clientList.forEach(client => {
+      console.log(`Sending ${advertisement} to ${client.firstName}`);
+    });
+  }
 }
 class Receipts {
   //  "Виручка": Каса збирає дані про виручку за день. Ці дані передаються в Бухгалтерію.
 }
 class AccountDepartment {
   //  "Виручка": Каса збирає дані про виручку за день. Ці дані передаються в Бухгалтерію.
-  private _kassa: Kassa = new Kassa();
   // Розпоряджається бюджетом, включно з оплатою співробітників,
-private _budget:ZooBudget = new ZooBudget();
+  private _account: Account = new Account();
+  private _employee: Employer[] = [];
+  private _animals: Animal[] = [];
+
+  get employee(): Employer[] {
+    return this._employee;
+  }
+  get animals(): Animal[] {
+    return this._animals;
+  }
+  public closeKassa(kassa: Kassa): void {
+    const daylyIncome = kassa.closeDay();
+    this._account.getPayment(daylyIncome, TransactionTypeEnum.KASSA);
+  }
+  public paySalaries(): void {
+    this._employee.forEach(employee => this._account.makePayment(employee.salary, TransactionTypeEnum.SALARY));
+    // this.balance -= employee.reduce((acc, employee)=>acc += employee.salary,0)
+  }
+  //  "Адміністрація": Відповідає за управління співробітниками і тваринами. Може додавати і видаляти співробітників і тварин.
+  public addListManager(item: Employer | Animal): void {
+    const list: (Employer | Animal)[] = isAnimal(item) ? this._animals : this._employee;
+    if (!list.includes(item)) {
+      list.push(item);
+    } else {
+      console.log('This item is already in the list');
+    }
+  }
+  public removeListManager(item: Employer | Animal): void {
+    const list: (Employer | Animal)[] = isAnimal(item) ? this._animals : this._employee;
+    const index = list.indexOf(item);
+    if (index != -1) {
+      list.splice(index, 1);
+    }
+  }
+  public generateReport(account: Account, typeOfReport: any): ITransactionRecord[] {
+    return account.createReport(new Date());
+  }
   //  закупівлею корму для тварин і обслуговуванням зоопарку. Зберігає дані про всіх співробітників, тварин і виплати.
   //  Можливість генерувати фінансові звіти.
 }
+
 class Administration {
   // Відповідає за управління співробітниками і тваринами. Може додавати і видаляти співробітників і тварин.
   //  Створює сповіщення про рекламні акції та інші важливі події в зоопарку.
+  addEmployer(accountDepartment: AccountDepartment, employer: Employer): void {
+    accountDepartment.addListManager(employer);
+  }
+  removeEmployer(accountDepartment: AccountDepartment, employer: Employer): void {
+    accountDepartment.removeListManager(employer);
+  }
+  addAnimal(accountDepartment: AccountDepartment, animal: Animal): void {
+    accountDepartment.addListManager(animal);
+  }
+  removeAnimal(accountDepartment: AccountDepartment, animal: Animal): void {
+    accountDepartment.removeListManager(animal);
+  }
 }
-class Animals {
+class Animal {
   private _name!: string;
   private _species!: string;
   private _age!: number;
   private _health: AnimalHealthType = 'good';
 }
-class Employee {}
-class ZooBudget {
-  private _ammount: number = 0;
-  public get ammount(): number {
-    return this._ammount;
+class Employer extends Person {
+  private _salary!: number;
+  set salary(ammount: number) {
+    this._salary = ammount;
   }
-  public debet(ammount: number): void {
-    this._ammount += ammount;
+  get salary(): number {
+    return this._salary;
   }
-  public credit(ammount: number): void {
-    this._ammount > ammount ? (this._ammount -= ammount) : console.log('Not enought money for transaction');
+}
+class Account {
+  protected _balance = 0;
+  protected readonly transactionHistory: TransactionHistory;
+  public get balance(): number {
+    return this._balance;
   }
+  constructor() {
+    this.transactionHistory = new TransactionHistory();
+  }
+  public createReport(date: Date): ITransactionRecord[] {
+    const list = this.transactionHistory.showTransactionHistory();
+    // some logic
+    return list.filter(transaction => transaction.date >= date);
+  }
+  public getPayment(ammount: number, transactionType: TransactionTypeEnum.KASSA | TransactionTypeEnum.DEBET): void {
+    this._balance += ammount;
+    this.recordTransaction(transactionType, ammount);
+  }
+  public makePayment(ammount: number, transactionType: TransactionTypeEnum): void {
+    if (this.balance >= ammount) {
+      this._balance -= ammount;
+      this.recordTransaction(transactionType, ammount);
+      //   paymentSystem.makePayment(amount);
+    } else console.log('Not enought money');
+  }
+  protected recordTransaction(transactionType: TransactionTypeEnum, amount: number): void {
+    const transaction: ITransactionRecord = { transactionType, amount, date: new Date() };
+    this.transactionHistory.addTransaction(transaction);
+  }
+}
+interface ITransactionRecord {
+  readonly transactionType: TransactionTypeEnum;
+  readonly amount: number;
+  readonly date: Date;
+}
+class TransactionHistory {
+  private readonly transactions: ITransactionRecord[] = [];
+
+  public addTransaction(transaction: ITransactionRecord): void {
+    this.transactions.push(transaction);
+  }
+  public showTransactionHistory(): ITransactionRecord[] {
+    return this.transactions;
+  }
+}
+function isAnimal(item: any): item is Animal {
+  return item instanceof Animal;
 }
